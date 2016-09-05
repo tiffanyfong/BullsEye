c1b21f6975a39900ff7f59f765de2db2883c1b9b
tiffanyfong <tiffanyfong42@gmail.com>
2016-09-04 18:10:51 -0700

    calculating slider value, starting to add target value

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue: Int = 0
+    var targetValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,15 +22,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var slider: UISlider!
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .Alert)
+        let message = "The slider's value is: \(currentValue)\nThe target value is: \(targetValue)"
         
-        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 }
 
